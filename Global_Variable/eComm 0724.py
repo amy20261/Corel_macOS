aa00049b6892439e364cbe8d6ba145b4e4f50df8
CORELCORP\amy.hsieh <amy.hsieh@corel.com>
2018-07-19 05:17:19 -0400

    0724 BTS sale

---
+++ Global_Variable/eComm 0724.py
@@ -0,0 +1,111 @@
+# Robotframework scripts on eComm project 
+# amy.hsieh@corel.com
+#https://docs.google.com/a/corel.com/spreadsheets/d/1uke1z4g2D-UDbuoePbx9OGUN4WyebOfkqhSOoyiSFWs/edit?usp=sharing
+
+#env='dev' or 'stg'
+env='dev'
+
+#micro website
+Corel='corel'
+wCDGS='coreldraw'
+wPTR='painterartist'
+wWPO='wordperfect'
+wPSP='paintshoppro'
+wVSP='videostudiopro'
+wASP='aftershotpro'
+
+#product
+CDGS='graphic-design-software'
+CAD='corel-cad'
+CDTS='technical-suite'
+CDHS='home-student'
+PTR='painter'
+PE='painter-essentials'
+particleshop='particleshop'
+particleshop='particleshop'
+WPOSTD='office-suite'
+WPOPRO='professional-edition'
+WPOHS='home-student'
+PDF='pdf-creator'
+PSP='paintshop-pro/standard'
+PSU='paintshop-pro/ultimate'
+PVB='photo-video-bundle'
+VSP='videostudio/pro'
+VSU='videostudio/ultimate'
+ASP='aftershot/pro'
+WinDVD='windvdpro'
+
+#PID for upgrade product
+UCDGS='ESDCDGS2018AMUG'
+UCAD='ESDCCAD2018MLUG'
+UCDTS='ESDCDTS2017MLUG'
+UCDHS='ESDCDHS2017MLUG'
+UPTR='ESDPTR2018MLUG'
+UPE='ESDPE6MLUG'
+Uparticleshop='ESDPTR2017MLUG'
+UWPOSTD='ESDWPX8STDENFRUG'
+UWPOPRO='ESDWPX8PROENFRUG'
+UWPOHS='ESDWPX8PROENFRUG'
+UPDF='ESDWPX8PROENFRUG'
+UPSP='ESDPSP2018MLUG'
+UPSU='ESDPSP2018ULMUG'
+UPVB='ESDPVS2018MLUG'
+UVSP='ESDVS2018PRMLUG'
+UVSU='ESDVS2018ULMLUG'
+UASP='ESDASP3MLPCUG'
+WD='ESDWD12PRMLUG'
+
+#link on Home page
+Corel_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_corel_0724.csv'
+CDGS_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_CDGS.csv'
+PTR_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_PTR.csv'
+WPO_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_WPO.csv'
+PSP_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_PSP_0724.csv'
+VSP_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_VSP_0724.csv'
+ASP_Home='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Home_ASP.csv'
+
+#link on SpOffer page
+Corel_SP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_sp_Corel.csv'
+CDGS_SP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Sp_CDGS.csv'
+PTR_SP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Sp_PTR.csv'
+WPO_SP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Sp_WPO.csv'
+PSP_SP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\\Link\\Checklink_Sp_PSP.csv'
+VSP_SP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Link\\Checklink_Sp_VSP.csv'
+
+
+#promo price on product page
+CDGS_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\CDGS00.csv'
+CAD_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\CAD00.csv'
+CDTS_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\CDTS05.csv'
+CDHS_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\CDHS15.csv'
+PTR_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\PTR00.csv'
+PE6_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\PE640.csv'
+bruch_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\Particleshop00.csv'
+Wstd_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\WPOSTD00.csv'
+Wpro_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\WPOPRO00.csv'
+#WHS_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\WPOHS50.csv'
+#PDF_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\PDF50.csv'
+PSU_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\PSU20.csv'
+PSP_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\PSP40.csv'
+PSB_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\PVB25.csv'
+VSU_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\VSU20.csv'
+VSP_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\VSP30.csv'
+ASP_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\ASP50.csv'
+DVD_file='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Data\\winDVD30.csv'
+
+#VA price file
+VA_particleshop='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_Particleshop.csv'
+VA_EBrushPack='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_EBrushPack.csv'
+VA_MBrushPack='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_MBrushPack.csv'
+VA_NBFX='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_NBFX.csv'
+
+#HTTPS files
+HTTPS_ASP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\HTTPS\\HTTPS_ASP.csv'
+HTTPS_WINDVD='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\HTTPS\\HTTPS_WINDVD.csv'
+HTTPS_WPO='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\HTTPS\\HTTPS_WPO.csv'
+HTTPS_COREL='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\HTTPS\\HTTPS_COREL.csv'
+
+
+#Cart
+cart_C='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Cart\\Condensed.csv'
+cart_L='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\Cart\\longform.csv'
