81d90945e3545a9ab64d46a40f3d3fc15b4a1452
CORELCORP\amy.hsieh <amy.hsieh@corel.com>
2018-07-23 07:57:40 -0400

    update VA

--- Global_Variable/eComm.py
+++ Global_Variable/eComm.py
@@ -98,7 +98,9 @@ VA_particleshop='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_Particlesh
 VA_EBrushPack='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_EBrushPack.csv'
 VA_MBrushPack='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_MBrushPack.csv'
 VA_NBFX='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_NBFX.csv'
+VA_GEBrushPack='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_GEBrushPack.csv'
 VA_TRAVEL='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_TRAVEL.csv'
+VA_SHBrushPack='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\VA\\VA_SHBrushPack.csv'
 
 #HTTPS files
 HTTPS_ASP='C:\\Users\\Amy.Hsieh\\Corel\\Global_Variable\\HTTPS\\HTTPS_ASP.csv'
